bb054cb3e54faf4d2ef487c53f5bb51a78d1d696
marcuspereiradev <vinnycoast@gmail.com>
2019-05-17 12:05:36 -0300

    first commit

---
+++ src/app.js
@@ -0,0 +1,14 @@
+'use strict'
+
+import React from 'react'
+import Title from './title';
+
+const App = React.createClass({
+  render: function () {
+    return (
+      <Title />
+    )
+  }
+})
+
+export default App
---
+++ src/title.js
@@ -0,0 +1,12 @@
+'use strict'
+import React from 'react'
+
+const Title = React.createClass({
+  render(){
+    return (
+      <h1>Olá Marcus!</h1>
+    )
+  }
+})
+
+export default Title
