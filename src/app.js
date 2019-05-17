129b65234a53b6f908bb2b236df5b9cf13a8b23b
marcuspereiradev <vinnycoast@gmail.com>
2019-05-17 16:44:39 -0300

    knowing the 'key' props

--- src/app.js
+++ src/app.js
@@ -2,11 +2,21 @@
 
 import React, { Component } from 'react'
 import Title from './title';
+import Square from './square'
 
 class App extends Component {
   render() {
     return (
-      <Title name="Marcus" lastName="Pereira" />
+      <div>
+        <Title name="Marcus" lastName="Pereira" />
+
+        <div className="container">
+          {['blue', 'red', 'green'].map((square, key) => (
+            console.log(`${square} ${key}`),
+            <Square color={square} key={key}/>
+          ))}
+        </div>
+      </div>
     )
   }
 }
