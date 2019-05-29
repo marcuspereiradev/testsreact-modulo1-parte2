d2fb1f111e1c71079731bfee0091c17a2daa758b
marcuspereiradev <vinnycoast@gmail.com>
2019-05-29 11:38:12 -0300

    propTypes

--- src/app.js
+++ src/app.js
@@ -1,36 +1,14 @@
 'use strict'
 
 import React, { Component } from 'react'
-import Timer from './timer'
+import Button from './button'
 
 class App extends Component {
-  constructor() {
-    super()
-    this.state = {
-      time: 0,
-      showTimer: true
-    }
-  }
-
-  componentWillMount() {
-    console.log('componentWillMount')
-  }
-
-  componentDidMount() {
-    console.log('componentDidMount')
-  }
-
+  
   render() {
-    console.log('render')
     return (
       <div>
-
-        {<Timer timer={this.state.time} />}
-        
-        <button onClick={() => {
-          this.setState({ time: this.state.time + 10 })
-        }}>Change Props</button>
-
+        <Button handleClick={() => console.log('Clicked')}>Click Me</Button>
       </div>
     )
   }
