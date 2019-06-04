a77cb8318c44c97abe6259fe3f731ec382e22e34
marcuspereiradev <vinnycoast@gmail.com>
2019-06-04 12:38:34 -0300

    Select and option

--- src/app.js
+++ src/app.js
@@ -7,19 +7,21 @@ class App extends Component {
   constructor() {
     super()
     this.state = {
-      value: 'valor inicial'
+      value: '2'
     }
   }
 
   render() {
     return (
-      <div>
-        <input type='text' value={this.state.value} onChange={(e) => {
-          this.setState({
-            value: e.target.value
-          })
-        }} />
-      </div>
+      <form>
+        <select value={this.state.value} onChange={(e) => {
+          this.setState({value: e.target.value})
+        }} >
+          <option value='1'>Opção 1</option>
+          <option value='2'>Opção 2</option>
+          <option value='3'>Opção 3</option>
+        </select>
+      </form>
     )
   }
 }
