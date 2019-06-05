0a32beaeebf3670cf5ab1f1ff67b58321592aad3
marcuspereiradev <vinnycoast@gmail.com>
2019-06-05 11:07:54 -0300

    Textarea with defaultValue

--- src/app.js
+++ src/app.js
@@ -7,20 +7,14 @@ class App extends Component {
   constructor() {
     super()
     this.state = {
-      value: '2'
+      value: 'textarea value2'
     }
   }
 
   render() {
     return (
       <form>
-        <select value={this.state.value} onChange={(e) => {
-          this.setState({value: e.target.value})
-        }} >
-          <option value='1'>Opção 1</option>
-          <option value='2'>Opção 2</option>
-          <option value='3'>Opção 3</option>
-        </select>
+        <textarea defaultValue={this.state.value}></textarea>
       </form>
     )
   }
